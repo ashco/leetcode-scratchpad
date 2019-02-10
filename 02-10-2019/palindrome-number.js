2d437bad48348495707fda1ab9de9766fca823d7
Ashton <ashchristie@gmail.com>
2019-02-10 14:39:43 -0800

    init commit

---
+++ 02-10-2019/palindrome-number.js
@@ -0,0 +1,57 @@
+﻿/**
+//  * @param {number} x
+//  * @return {boolean}
+ */
+// var isPalindrome = function(x) {
+//   const str = x.toString();
+//   const N = str.length - 1;
+//   for (let i = 0; i <= N / 2; i++) {
+//     if (str[i] !== str[N - i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+// var isPalindrome = function(x) {
+//   if (x < 0) {
+//     return false;
+//   }
+
+//   let num = x;
+//   const arr = [];
+//   while (num >= 1) {
+//     const tail = num % 10;
+//     arr.push(tail);
+//     num = Math.floor(num / 10);
+//   }
+
+//   const revNum = parseInt(arr.join(''));
+
+//   return x === revNum;
+// };
+
+var isPalindrome = function(x) {
+  // Special cases
+  if (x < 0) return false;
+  if (x < 10) return true;
+  if (x % 10 === 0) return false;
+
+  let rev = 0;
+  while (rev < x) {
+    // add to rev
+    rev = rev * 10;
+    rev = rev + (x % 10);
+    // remove from x
+    x = Math.floor(x / 10);
+  }
+  return x === rev || x === Math.floor(rev / 10);
+};
+
+const x = 1121211;
+const y = 1312222131;
+// const str = 'Hello World';
+
+console.log(isPalindrome(x));
+console.log(isPalindrome(y));
+// console.log(isPalindrome(str));
---
+++ 02-10-2019/two-sum.js
@@ -0,0 +1,32 @@
+﻿// @param {number[]} nums
+// @param {number} target
+// @return {number[]}
+
+// var twoSum = function(nums, target) {
+//   for (var i = 0; i < nums.length; i++) {
+//     for (var j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
+
+var twoSum = function(nums, target) {
+  const map = {};
+
+  for (let i = 0; i <= nums.length - 1; i++) {
+    const val = nums[i];
+    const gap = target - val; // Calc what other value is needed
+
+    if (map[gap] !== undefined) {
+      return [map[gap], i];
+    }
+    map[val] = i;
+  }
+};
+
+const nums = [2, 7, 11, 15],
+  target = 18;
+
+console.log(twoSum(nums, target));
